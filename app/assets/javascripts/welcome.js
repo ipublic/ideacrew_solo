8a2738ad2cd44bd3935797e53c406e2791297529
Dan Thomas <dan.thomas@dc.gov>
2015-01-11 13:16:32 -0500

    major site content revisions Add hover and click animation events

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -5,10 +5,48 @@ $(document).ready(function() {
     changeHash: false
   });
 
-  $('#about-header').one('inview', function() {
+  $('#services-header').one('inview', function() {
     $(this).addClass('animated fadeInUp');
   });
+
+  animationHover('#web-technology-service', 'tada');
+  animationHover('#operations-support-service', 'tada');
+  animationHover('#consulting-service', 'tada');
+
+  animationHover('.playbook', 'pulse');
+  animationHover('.playbook', 'pulse');
+
+  function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+      function() {
+        element.addClass('animated ' + animation);
+      },
+      function(){
+        //wait for animation to finish before removing classes
+        window.setTimeout( function(){
+          element.removeClass('animated ' + animation);
+        }, 2000);
+      }
+    );
+  };
+
+  function animationClick(element, animation){
+    element = $(element);
+    element.click(
+      function() {
+        element.addClass('animated ' + animation);
+        //wait for animation to finish before removing classes
+        window.setTimeout( function(){
+            element.removeClass('animated ' + animation);
+        }, 2000);
+      }
+    );
+  };
+
 });
+
+
 
 // $(document).ready(function() {
 //   $('#top-nav').onePageNav({

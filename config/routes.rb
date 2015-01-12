7fdaf1c509f7b902b418322f0ce1c199da272356
Dan Thomas <dan.thomas@dc.gov>
2015-01-12 07:41:33 -0500

    Add contact_us route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   root :to => 'welcome#index'
   get 'welcome/index'
-  get 'welcome/contact'
+  get "welcome/contact_us"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

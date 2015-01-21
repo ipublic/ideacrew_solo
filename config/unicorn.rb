ad565e226152fafe06152a491200b599b4b93ad8
Dan Thomas <dan.thomas@dc.gov>
2015-01-20 22:26:56 -0500

    initial commit

---
+++ config/unicorn.rb
@@ -0,0 +1,8 @@
+working_directory "/var/www/ideacrew/current"
+pid "/var/www/ideacrew/shared/pids/unicorn.pid"
+stderr_path "/var/www/ideacrew/shared/log/unicorn.log"
+stdout_path "/var/www/ideacrew/shared/log/unicorn.log"
+
+listen "/tmp/unicorn.ap.sock"
+worker_processes 2
+timeout 180

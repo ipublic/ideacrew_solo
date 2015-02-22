1760c1d3e02491bb54fe3b390ed8d6cb5f7158ae
Dan Thomas <dan.thomas@dc.gov>
2015-02-22 11:22:59 -0500

    remove kruft

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -13,12 +13,3 @@ timeout 30
 before_exec do |_|
   ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
 end
-
-# working_directory "/var/www/ideacrew/current"
-# pid "/var/www/ideacrew/shared/pids/unicorn.pid"
-# stderr_path "/var/www/ideacrew/shared/log/unicorn.log"
-# stdout_path "/var/www/ideacrew/shared/log/unicorn.log"
-
-# listen "/tmp/unicorn.ap.sock"
-# worker_processes 2
-# timeout 180
